207e89a0c1ed7f341fcea8f4f03c6eec0a7e1509
unknown <ballourk@gmail.com>
2023-03-23 23:42:46 +0200

    show-more/show-less

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,12 @@ const url = 'https://course-api.com/react-tours-project';
 const App = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [tours, setTours] = useState([]);
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
+
   const getTours = async () => {
     setIsLoading(true);
     try {
@@ -30,7 +36,7 @@ const App = () => {
   if (tours)
     return (
       <main>
-        <Tours tours={tours} />
+        <Tours tours={tours} removeTour={removeTour} />
       </main>
     );
 };
